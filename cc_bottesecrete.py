376fdf1265d10bdb649d06e90fb2df80ea9a1b49
plapla-1357 <73687455+plapla-1357@users.noreply.github.com>
2020-12-09 20:01:19 +0100

    update

--- cc_bottesecrete.py
+++ cc_bottesecrete.py
@@ -1,1 +1,30 @@
+from random import*
+
+def make_pairs_boucle(names):
+    for i in range(len(names)-1):
+        print(f"{names[i]} va donner un cadeau a {names[i+1]}")
+    print(f"{names[len(names)-1]} va donner un cadeau a {names[0]}")
+
+def make_pairs(names):
+    
+    prs_choisi = []
+    list_paire = {}
+    for j in range(len(names)):
+        finis = False
+        while not finis:
+            prs1 = randint(-1,len(names)-1)
+            prs2 = randint(-1,len(names)-1)
+            if prs1 != prs2 and prs2 not in prs_choisi:
+                finis = True 
+                prs_choisi.append(prs2)
+                #list_paire[names[pers1]]=names[pers2]
+                paire = [names[prs1],names[prs2]]
+                print(f"{paire[0]} va donner un cadeau a {paire[1]}")
+    
+
+
+names = input("Entre les noms des participant(e)s, séparés par des virgules:")
+names_new = names.split(",")
+
+make_pairs(names_new)
 
