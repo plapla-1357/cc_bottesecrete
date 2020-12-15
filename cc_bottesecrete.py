aad3110aa693e90f5d201478a3984d2ccdfdb014
plapla-1357 <73687455+plapla-1357@users.noreply.github.com>
2020-12-15 18:18:11 +0100

    finis
    le principal est finis

--- cc_bottesecrete.py
+++ cc_bottesecrete.py
@@ -1,30 +1,28 @@
 from random import*
 
+list_formulation = [ "va rendre heureux :","sera dans l'obligation de se demener pour donner un present a :",
+    "va donner un cadeaux a :",
+    "va jouer le père noël pour :",
+    "deviendras, grace a son cadeau très bon ami avec :"]
+
 def make_pairs_boucle(names):
     for i in range(len(names)-1):
-        print(f"{names[i]} va donner un cadeau a {names[i+1]}")
-    print(f"{names[len(names)-1]} va donner un cadeau a {names[0]}")
+        print(f"{names[i]} {formulation_rd()} {names[i+1]}")
+    print(f"{names[len(names)-1]} {formulation_rd()} {names[0]}")
 
 def make_pairs(names):
-    
-    prs2_choisi = []
-    #list_paire = {}
-    for j in range(len(names)):
-        finis = False
-        while not finis:
-            prs1 = randint(-1,len(names)-1)
-            prs2 = randint(-1,len(names)-1)
-            if prs1 != prs2 and prs2 not in prs_choisi:
-                finis = True 
-                prs2_choisi = prs_choisi.append(prs2)
-                #list_paire[names[pers1]]=names[pers2]
-                paire = [names[prs1],names[prs2]]
-                print(f"{paire[0]} va donner un cadeau a {paire[1]}")
-    
+    shuffle(names)
+    return names
+
+def formulation_rd():
+    nb = randint(0,len(list_formulation)-1)
+    return list_formulation[nb]
+        
+
 
 
 names = input("Entre les noms des participant(e)s, séparés par des virgules:")
 names_new = names.split(",")
 
-make_pairs(names_new)
+make_pairs_boucle(make_pairs(names_new))
 
