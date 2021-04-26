32971d624b222f71b4fde2e69608a29116a64634
plapla-1357 <73687455+plapla-1357@users.noreply.github.com>
2021-04-26 15:39:19 +0200

    save

--- cc_bottesecrete.py
+++ cc_bottesecrete.py
@@ -6,9 +6,20 @@ list_formulation = [ "va rendre heureux :","sera dans l'obligation de se demener
     "deviendras, grace a son cadeau très bon ami avec :"]
 
 def make_pairs_boucle(names):
+    code = []
+    br = "<br>"
     for i in range(len(names)-1):
-        print(f"{names[i]} {formulation_rd()} {names[i+1]}")
-    print(f"{names[len(names)-1]} {formulation_rd()} {names[0]}")
+        msg = f"{names[i]} {formulation_rd()} {names[i+1]}"
+        code.append(msg)  
+        code.append(br)
+        print(msg)
+    msg = f"{names[len(names)-1]} {formulation_rd()} {names[0]}"
+    print(msg)
+    code.append(msg)
+    code.append(br)
+    coden = " ".join(code)
+    print(coden)
+    return coden
 
 def make_pairs(names):
     shuffle(names)
@@ -24,5 +35,24 @@ def formulation_rd():
 names = input("Entre les noms des participant(e)s, séparés par des virgules:")
 names_new = names.split(",")
 
-make_pairs_boucle(make_pairs(names_new))
 
+
+html = f'''
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <!--<script src="script.js"></script>-->
+</head>
+<body>
+ {make_pairs_boucle(make_pairs(names_new))}
+</body>
+</html>
+'''
+
+
+fhtml = open("html.html","w")
+fhtml.write(html)
+fhtml.close()
